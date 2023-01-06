54500145ec3435d6669c261699146a8d8951b0d1
Noocktall Klown S <nklowns@gmail.com>
2023-01-06 02:46:30 -0300

    feat: bump versions ncu -u

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+    proxy: {
+      "/socket": {
+        target: "wss://cloudlessv.netlify.app/",
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/socket/, ""),
+      },
+      "/api": {
+        target: "https://cloudlessv.netlify.app/.netlify/functions",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
